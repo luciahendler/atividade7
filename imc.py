aaca19fcc2d66d71c086128dc5e2ce69c7e59fb0
Lucia Hendler <luciahendler@gmail.com>
2020-03-29 20:34:55 -0300

    Digitacao do codigo

--- imc.py
+++ imc.py
@@ -1,1 +1,6 @@
-﻿
+﻿peso = float(input("Digite seu peso, em quilogramas: "))
+altura = float(input("Digite sua altura, em metros e com separador '.': "))
+
+imc = peso/(altura**2)
+
+print("Seu IMC é: ", round(imc,2))
